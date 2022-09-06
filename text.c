88bf11f1bef306b9d4966fb5c5185806e6022b35
drull1000 <pcaetanop@protonmail.com>
2022-09-06 07:46:53 -0300

    adding another exercise

--- text.c
+++ text.c
@@ -1,63 +1,40 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
 
-
 int main (){
-
     FILE *filePointer;
-    int lines;
-    int characters;
-    int pritable;
-    char fileName[18]="";
 
-    scanf("%s", &fileName[18]);
-    if (!(filePointer = fopen(fileName[18], "rt"))){
-        printf("Erro no arquivo %s\n", &fileName);
-        exit(1);
+    int numberOfLines=0;
+    int numberOfCharacters=0;
+    int numberOfPrintable=0;
+
+    char character;
+    char fileName[18];
+
+    scanf("%s", fileName);
+
+    if (!(filePointer = fopen(fileName, "r"))){
+        printf("Erro no arquivo %s\n", fileName);
+        return 0;
     }
 
-    while(fgets()
-    fgets(image.magicNumber, 4, filePointer);
-    /*fgets(image.description, 100, filePointer);*/
-
-    if(fscanf(filePointer,"%d %d",&image.columns, &image.lines)!=2){
-      perror("Aquivo corrompido.\n");
-      return -4;
-   }
-
-    if(fscanf(filePointer,"%d",&image.max)!=1){
-        perror("Aquivo corrompido.\n");
-      return -4;
-        return -4;
-    }
-
-    unsigned int * bitmap = malloc(image.columns*image.lines*4);
-    unsigned int *iterator = bitmap;
-
-    for (int i = 0; i < image.lines; i++){
-        for (int j = 0; j < image.columns; j++, iterator++){
-            fscanf(filePointer,"%u",iterator);
+    for (character = getc(filePointer); character != EOF; character = getc(filePointer)){
+        if (character == '\n'){
+            numberOfLines++;
         }
-    }
-
-    if (fclose (filePointer)==EOF){
-        perror("Arquivo não fecha.\n");
-        exit(1);
-    };
-
-    for (int i = 0; i < image.lines; i++){
-        for (int j = 0; j < image.columns; j++, bitmap++){
-            if(j==0){
-                sprintf(hex,"%X", *bitmap);
-                printf("%s", hex);
-            }
-            else{
-                sprintf(hex,"%3X", *bitmap);
-                printf("%s", hex);
-            }
+        if (isprint(character)){
+            numberOfPrintable++;
         }
-        printf("\n");
+        numberOfCharacters++;
     }
+
+    fclose(filePointer);
+
+    printf("%d\n", numberOfCharacters);
+    printf("%d\n", numberOfPrintable);
+    printf("%d\n", numberOfLines);
+
+    return 0;
 }
-
