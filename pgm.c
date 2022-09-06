aa8b0606096a54b4bb60552d21c8d30ed343c8d8
drull1000 <pcaetanop@protonmail.com>
2022-09-06 11:39:58 -0300

    adding another exercise

--- pgm.c
+++ pgm.c
@@ -2,42 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct image{
+    char magicNumber[4];
+    char description[100];
+    int max;
+    int columns;
+    int lines;
+} image;
+
+void closeFile(FILE *filePointer);
+void getHeaders(FILE *filePointer);
+unsigned int* readImage(char fileName);
+void printImage(unsigned int* bitmap);
+
 int main (){
-    FILE *filePointer;
     char hex[5];
-
-    struct image{
-        char magicNumber[4];
-        char description[100];
-        int max;
-        int columns;
-        int lines;
-    } image;
-
     char fileName[18];
-    scanf("%s", fileName);
+    unsigned int* image = readImage(*fileName);
+    printImage(image);
+    free(image);
+}
 
-    if (!(filePointer = fopen(fileName, "rt"))){
+unsigned int* readImage(char fileName){
+    FILE *filePointer = NULL;
+    scanf("%s", &fileName);
+
+    if (!(filePointer = fopen(&fileName, "rt"))){
         printf("Erro: arquivo inexistente\n");
-        return 0;
-
+        exit(0);
     }
-
-    fgets(image.magicNumber, 4, filePointer);
-    fgets(image.description, 100, filePointer);
+    getHeaders(filePointer);
 
     if(fscanf(filePointer,"%d %d",&image.columns, &image.lines)!=2){
       perror("Aquivo corrompido.\n");
-      return -4;
    }
 
     if(fscanf(filePointer,"%d",&image.max)!=1){
         perror("Aquivo corrompido.\n");
-      return -4;
-        return -4;
     }
 
-    unsigned int * bitmap = malloc(image.columns*image.lines*4);
+    unsigned int *bitmap = malloc(image.columns*image.lines*4);
     unsigned int *iterator = bitmap;
 
     for (int i = 0; i < image.lines; i++){
@@ -45,23 +49,34 @@ int main (){
             fscanf(filePointer,"%u",iterator);
         }
     }
+    closeFile(filePointer);
+    return bitmap;
+}
 
+void getHeaders(FILE *filePointer){
+    fgets(image.magicNumber, 4, filePointer);
+    fgets(image.description, 100, filePointer);
+}
+
+void printImage(unsigned int* bitmap){
+    for (int i = 0; i < image.lines; i++){
+        for (int j = 0; j < image.columns; j++, bitmap++){
+            if(j<image.columns-1){
+                printf("%3X ", *bitmap);
+            }
+            else{
+                printf("%3X", *bitmap);
+            }
+        }
+        if(i != image.lines-1){
+            printf("\n");
+        }
+    }
+}
+
+void closeFile(FILE *filePointer){
     if (fclose (filePointer)==EOF){
         perror("Arquivo não fecha.\n");
         exit(1);
-    };
-
-    for (int i = 0; i < image.lines; i++){
-        for (int j = 0; j < image.columns; j++, bitmap++){
-            if(j==image.lines){
-                sprintf(hex,"%3X", *bitmap);
-                printf("%s", hex);
-            }
-            else{
-                sprintf(hex,"%3X ", *bitmap);
-                printf("%s", hex);
-            }
-        }
-        printf("\n");
     }
 }
